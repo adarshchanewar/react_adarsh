29ac0e38e239b616093a5ccc91590b30c4027699
adarshchanewar <1adarsh2chanewar@gmail.com>
2023-08-24 19:39:06 +0530

    adding through vs code

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const heading=React.createElement(
     "h1",
     {id:"heading"},
-    "hello world from react from hinjewadi!!");
+    "hello world from react from hinjewadi,pune!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
